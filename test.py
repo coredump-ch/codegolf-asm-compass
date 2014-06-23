ed775d005f6bdad819f2dc43ee5455b9ceabb267
Danilo Bargen <mail@dbrgn.ch>
2014-06-24 01:43:02 +0200

    Added --short option to test.py

--- test.py
+++ test.py
@@ -8,12 +8,17 @@ import StringIO
 
 out = StringIO.StringIO()
 
+short = False
+if '--short' in sys.argv:
+    short = True
+    sys.argv.remove('--short')
+
 if len(sys.argv) == 1:
     path = './main'
 elif len(sys.argv) == 2:
     path = sys.argv[2]
 else:
-    print('Usage: python test.py [path]')
+    print('Usage: python test.py [path] [--short]')
     sys.exit(-1)
 
 for i in range(8):
@@ -26,8 +31,12 @@ for i in range(8):
 
 with open('expected.txt', 'r') as f:
     if f.read() == out.getvalue():
-        print('Success!')
-        print('Binary size is %s bytes.' % os.path.getsize('./main'))
+        size = os.path.getsize('./main')
+        if short:
+            print(size)
+        else:
+            print('Success!')
+            print('Binary size is %s bytes.' % size)
         sys.exit(0)
 
 print('Invalid output.')
