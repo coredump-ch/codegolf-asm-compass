41baec96d23065ec2afd24304035af155e5048a0
Felix Morgner <felix.morgner@gmail.com>
2016-11-12 22:32:19 +0100

    Better cheat prevention
    In prior versions it was possible to cheat by simply indexing blockwise
    into the expected.txt file an printing the desired block. This
    changeset first of all generates the expected strings on the fly and
    secondly randomizes the order (just because we can).

--- test.py
+++ test.py
@@ -1,12 +1,32 @@
 # -*- coding: utf-8 -*-
-from __future__ import print_function, division, absolute_import, unicode_literals
+from __future__ import print_function
 
 import os
 import sys
 import subprocess
-import StringIO
+import random
 
-out = StringIO.StringIO()
+COMPASS = (
+    '   _N_\n' +
+    '  /   \\\n' +
+    'W|  .  |E\n' +
+    ' |     |\n' +
+    '  \\_ _/\n' +
+    '    S\n'
+)
+
+
+def compass(direction):
+    return {
+        0: lambda x: x[:11] + "|" + x[12:],
+        1: lambda x: x[:20] + "/" + x[21:],
+        2: lambda x: x[:20] + "_" + x[21:],
+        3: lambda x: x[:30] + "\\" + x[31:],
+        4: lambda x: x[:38] + "|" + x[39:],
+        5: lambda x: x[:28] + "/" + x[29:],
+        6: lambda x: x[:18] + "_" + x[19:],
+        7: lambda x: x[:18] + "\\" + x[19:]
+    }[direction](COMPASS)
 
 short = False
 if '--short' in sys.argv:
@@ -21,23 +41,21 @@ else:
     print('Usage: python test.py [path] [--short]')
     sys.exit(-1)
 
-for i in range(8):
+args = range(8)
+random.shuffle(args)
+size = os.path.getsize('./main')
+for i in args:
     try:
         output = subprocess.check_output([path, str(i)])
+        if output != compass(i):
+            print('Invalid output.')
+            sys.exit(1)
     except subprocess.CalledProcessError as e:
         print('Error calling binary with argument %d: %s' % (i, e))
         sys.exit(-1)
-    out.write(output)
 
-with open('expected.txt', 'r') as f:
-    if f.read() == out.getvalue():
-        size = os.path.getsize('./main')
-        if short:
-            print(size)
-        else:
-            print('Success!')
-            print('Binary size is %s bytes.' % size)
-        sys.exit(0)
-
-print('Invalid output.')
-sys.exit(1)
+if short:
+    print(size)
+else:
+    print('Success!')
+    print('Binary size is %s bytes.' % size)
